071bd7b4c0a06cd7004e287d6b0ee1b55e0ae795
Saurabh-Linge <saurabh.linge.dev@gmail.com>
2026-05-05 16:37:25 +0530

    Removed unnecessary folders

--- src/app/features/employees/employees.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
-
-export const EMPLOYEE_ROUTES: Routes = [
-  {
-    path: '',
-    component: EmployeeListComponent,
-  }
-];
